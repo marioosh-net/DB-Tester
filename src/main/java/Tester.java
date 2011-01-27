10f55a4532d44208f7efdd8c372dcadf86f5e221
marioosh <marioosh@963358be-e051-4187-90f4-1164e00299d6>
2011-01-27 12:25:08 +0000

    git-svn-id: file:///var/svn/java/DBTester@1152 963358be-e051-4187-90f4-1164e00299d6

---
+++ src/main/java/Tester.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+import org.apache.log4j.Logger;
+
+public class Tester {
+
+	private Logger log = Logger.getLogger(getClass());
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Tester();
+	}
+
+	public Tester() {
+		try {
+			Class.forName("org.h2.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "");
+			log.debug(conn);
+
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			log.error(e);
+		} catch (SQLException e) {
+			log.error(e);
+		}
+
+	}
+}
